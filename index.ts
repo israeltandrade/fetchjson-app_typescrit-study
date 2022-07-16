5b7317b21394db0e14337ca8697e4a65d137f9d7
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-16 00:16:10 -0300

    object into variables

--- index.ts
+++ index.ts
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-// Módulo axios usa método get para buscar informações na url.
-// Na sequência, assim que a promise é cumprida, então (then) vem
-// a resposta (response) do módulo axios, passada como parâmetro
-// da arrow function, que resulta num console.log.
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data;
+
+    // Desta maneira os dados são retornados como "undefined"
+    const ID = todo.ID;
+    const title = todo.Title;
+    const finished = todo.finished;
+
+    console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+    `);
 });
