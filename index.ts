7c580d8c47f8296819155200e3181ca7d5cae522
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-16 23:54:10 -0300

    add function and types of parameters

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-// A interface a seguir permite perceber os erros durante o desenvolvimento:
 interface Todo {
     id: number;
     title: string;
@@ -10,30 +9,19 @@ interface Todo {
 }
 
 axios.get(url).then(response => {
-    // Associação da resposta com a interface Todo:
     const todo = response.data as Todo;
 
-    // Os dados vem undefined porque o nome das propriedades está com case sensitive errors.
-    // Agora que a interface está associada, a IDE aponta os problemas com as propriedades. 
-    // const ID = todo.ID;
-    // const title = todo.Title;
-    // const finished = todo.finished;
-
-    // console.log(`
-    // The Todo with ID: ${ID}
-    // Has a title of: ${title}
-    // Is it finished? ${finished}
-    // `);
-
-    // Código reformulado em face das informações da interface.
     const id = todo.id;
     const title = todo.title;
     const completed = todo.completed;
 
+    logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
     console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it completed? ${completed}
     `);
-
-});
+};
